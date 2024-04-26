003e4f1ec1091bacfbd7c7310c3bf58b84dc6e5e
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-04-25 17:26:13 -0700

    Add files via upload

---
+++ examples/OttoS_BLE/OttoS_BLE.ino
@@ -0,0 +1,294 @@
+/*  
+*                        
+*    ______________      ____                                _____    _  _     _
+*   |   __     __  |    / __ \ _________ _________   ____   |  __ \  | | \\   //  
+*   |  |__|   |__| |   | |  | |___   ___ ___   ___  / __ \  | |  | | | |  \\ //  
+*   |_    _________|   | |  | |   | |       | |    | |  | | | |  | | | |   | |
+*   | \__/         |   | |__| |   | |       | |    | |__| | | |__| | | |   | |
+*   |              |    \____/    |_|       |_|     \____/  |_____/  |_|   |_|
+*   |_    _________|
+*     \__/            
+*
+*    This Sketch was created to control Otto Starter with the Offical Web Bluetooth Controller for Otto DIY Robots.
+*    For any question about this script you can contact us at education@ottodiy.com
+*    By: Iván R. Artiles
+*/
+
+#include <Otto.h>
+#include <EEPROM.h>
+
+#define LEFTLEG 2
+#define RIGHTLEG 3
+#define LEFTFOOT 4
+#define RIGHTFOOT 5
+#define TRIG 8
+#define ECHO 9
+#define BLE_TX 11
+#define BLE_RX 12
+#define BUZZER 13
+
+#if defined(ARDUINO_ARCH_ESP32)
+  #include "BluetoothSerial.h"
+  String device_name = "Otto BT Esp32";
+  BluetoothSerial bluetooth;
+#else
+  #include <SoftwareSerial.h>
+  String device_name = "Otto BT";
+  SoftwareSerial bluetooth(BLE_TX, BLE_RX);
+#endif
+
+int move_speed[] = {3000, 2000, 1000, 750, 500, 250};
+int n = 2;
+int ultrasound_threeshold = 15;
+String command = "";
+
+int v;
+int ch;
+int i;
+int positions[] = {90, 90, 90, 90};
+int8_t trims[4] = {0,0,0,0};
+unsigned long sync_time = 0;
+
+bool calibration = false;
+  
+Otto Ottobot;
+
+long ultrasound_distance() {
+   long duration, distance;
+   digitalWrite(TRIG,LOW);
+   delayMicroseconds(2);
+   digitalWrite(TRIG, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(TRIG, LOW);
+   duration = pulseIn(ECHO, HIGH);
+   distance = duration/58;
+   return distance;
+}
+
+void setup() {
+  Serial.begin(9600);
+  Ottobot.init(LEFTLEG, RIGHTLEG, LEFTFOOT, RIGHTFOOT, true, BUZZER);
+  pinMode(TRIG, OUTPUT); 
+  pinMode(ECHO, INPUT);
+
+#if defined(ARDUINO_ARCH_ESP32)
+  bluetooth.begin(device_name);
+  //bluetooth.deleteAllBondedDevices(); // Uncomment this to delete paired devices; Must be called after begin
+#else
+  bluetooth.begin(9600);
+  bluetooth.print("AT+NAME" + device_name);
+#endif
+  
+  Ottobot.home();
+  v = 0;
+}
+
+void loop() {
+  checkBluetooth();//if something is coming at us
+  if (command == "forward") {
+    Forward();
+  }
+  else if (command == "backward") {
+    Backward();
+  }
+  else if (command == "right") {
+    Right();
+  }
+  else if (command == "left") {
+    Left();
+  }
+  else if (command == "avoidance") {
+    Avoidance();
+  }
+  else if (command == "force") {
+    UseForce();
+  }
+}
+
+void checkBluetooth() {
+  char charBuffer[20];//most we would ever see
+  
+  if (bluetooth.available() > 0) {
+    int numberOfBytesReceived = bluetooth.readBytesUntil('\n', charBuffer, 19);
+    charBuffer[numberOfBytesReceived] = NULL;
+    Serial.print("Received: ");
+    Serial.println(charBuffer);
+    
+    n = charBuffer[numberOfBytesReceived-1]-'0';
+    
+    if (strstr(charBuffer, "forward") == &charBuffer[0]) {
+      command = "forward";
+    }   
+    else if (strstr(charBuffer, "backward") == &charBuffer[0]) {
+      command = "backward";
+    }
+    else if (strstr(charBuffer, "right") == &charBuffer[0]) {
+      command = "right";
+    }
+    else if (strstr(charBuffer, "left") == &charBuffer[0]) {
+      command = "left";
+    }
+    else if (strstr(charBuffer, "stop") == &charBuffer[0]) {
+      command = "stop";
+      Stop();
+    }
+    else if (strstr(charBuffer, "ultrasound") == &charBuffer[0]) {
+      Stop();
+      bluetooth.print(ultrasound_distance());
+    }
+    else if (strstr(charBuffer, "avoidance") == &charBuffer[0]) {
+      command = "avoidance";
+    }
+    else if (strstr(charBuffer, "force") == &charBuffer[0]) {
+      command = "force";
+    }
+    else if (strstr(charBuffer, "happy") == &charBuffer[0]) {
+      command = "";
+      Ottobot.playGesture(OttoSuperHappy);
+    }
+    else if (strstr(charBuffer, "victory") == &charBuffer[0]) {
+      command = "";
+      Ottobot.playGesture(OttoVictory);
+    }
+    else if (strstr(charBuffer, "sad") == &charBuffer[0]) {
+      command = "";
+      Ottobot.playGesture(OttoSad);
+    }
+    else if (strstr(charBuffer, "sleeping") == &charBuffer[0]) {
+      command = "";
+      Ottobot.playGesture(OttoSleeping);
+    }
+    else if (strstr(charBuffer, "confused") == &charBuffer[0]) {
+      command = "";
+      Ottobot.playGesture(OttoConfused);
+    }
+    else if (strstr(charBuffer, "fail") == &charBuffer[0]) {
+      command = "";
+      Ottobot.playGesture(OttoFail);
+    }
+    else if (strstr(charBuffer, "fart") == &charBuffer[0]) {
+      command = "";
+      Ottobot.playGesture(OttoFart);
+    }
+    else if (strstr(charBuffer, "C") == &charBuffer[0]) {
+      
+      if (calibration == false) {
+        Ottobot._moveServos(10, positions);
+        calibration = true;
+        delay(50);
+      } 
+      command = "calibration";
+      Calibration(charBuffer);
+    }
+    else if (strstr(charBuffer, "walk_test") == &charBuffer[0]) {
+      command = "";
+      Ottobot.walk(3, 1000, FORWARD);
+    }
+    else if (strstr(charBuffer, "save_calibration") == &charBuffer[0]) {
+      command = "";
+      readChar('s');
+    }
+  }
+}
+
+void Forward() {
+  Ottobot.walk(1, move_speed[n], FORWARD);
+}
+
+void Backward() {
+  Ottobot.walk(1, move_speed[n], BACKWARD);
+}
+
+void Right() {
+  Ottobot.walk(1, move_speed[n], RIGHT);
+}
+
+void Left() {
+  Ottobot.walk(1, move_speed[n], LEFT);
+}
+
+void Stop() {
+  Ottobot.home();
+}
+
+void Avoidance() {
+  if (ultrasound_distance() <= ultrasound_threeshold) {
+    Ottobot.playGesture(OttoConfused);
+    for (int count=0 ; count<2 ; count++) {
+      Ottobot.walk(1,move_speed[n],-1); // BACKWARD
+    }
+    for (int count=0 ; count<4 ; count++) {
+      Ottobot.turn(1,move_speed[n],1); // LEFT
+    }
+  }
+  Ottobot.walk(1,move_speed[n],1); // FORWARD
+}
+
+void UseForce() {
+  if (ultrasound_distance() <= ultrasound_threeshold) {
+      Ottobot.walk(1,move_speed[n],-1); // BACKWARD
+    }
+    if ((ultrasound_distance() > 10) && ( ultrasound_distance() < 15)) {
+      Ottobot.home();
+    }
+    if ((ultrasound_distance() > 15) && ( ultrasound_distance() < 30)) {
+      Ottobot.walk(1,move_speed[n],1); // FORWARD
+    }
+    if (ultrasound_distance() > 30) {
+      Ottobot.home();
+    }  
+}
+
+void Settings(String ts_ultrasound) {
+  ultrasound_threeshold = ts_ultrasound.toInt();
+}
+
+void Calibration(String c) {
+  if (sync_time < millis()) {
+      sync_time = millis() + 50;
+      for (int k = 1; k < c.length(); k++) {
+        readChar((c[k]));
+      }
+  } 
+}
+
+void readChar(char ch) {
+  switch (ch) {
+  case '0'...'9':
+    v = (v * 10 + ch) - 48;
+    break;
+   case 'a':
+    trims[0] = v-90;
+    setTrims();
+    v = 0;
+    break;
+   case 'b':
+    trims[1] = v-90;
+    setTrims();
+    v = 0;
+    break;
+   case 'c':
+    trims[2] = v-90;
+    setTrims();
+    v = 0;
+    break;
+   case 'd':
+    trims[3] = v-90;
+    setTrims();
+    v = 0;
+    break;
+   case 's':
+    for (i=0 ; i<=3 ; i=i+1) {
+      EEPROM.write(i,trims[i]);
+    }
+    delay(500);
+    Ottobot.crusaito(1, 1000, 25, -1);
+    Ottobot.crusaito(1, 1000, 25, 1);
+    break;
+  }
+}
+
+void setTrims() {
+  Ottobot.setTrims(trims[0],trims[1],trims[2],trims[3]);
+  Ottobot._moveServos(10, positions); 
+}
